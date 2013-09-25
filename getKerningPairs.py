dcd54ad04d0381c2bb2d4b764c9dab1c8517ac63
Frank Grießhammer <fgriessh@adobe.com>
2013-09-24 18:14:32 -0700

    identical result, code less complex.

--- getKerningPairs.py
+++ getKerningPairs.py
@@ -23,6 +23,7 @@ class myLeftClass:
         self.glyphs = []
         self.class1Record = 0
 
+
 class myRightClass:
     def __init__(self):
         self.glyphs = []
@@ -32,7 +33,8 @@ class myRightClass:
 def collectUniqueKernLookupListIndexes(featureRecord):
     uniqueKernLookupIndexList = []
     for featRecItem in featureRecord:
-        # print featRecItem.FeatureTag  # GPOS feature tags (e.g. kern, mark, mkmk, size) of each ScriptRecord
+        # print featRecItem.FeatureTag  
+        # GPOS feature tags (e.g. kern, mark, mkmk, size) of each ScriptRecord
         if featRecItem.FeatureTag == kKernFeatureTag:
             feature = featRecItem.Feature
 
@@ -43,89 +45,124 @@ def collectUniqueKernLookupListIndexes(featureRecord):
     return uniqueKernLookupIndexList
 
 
-def getPairKerning():
-    pass
+class Analyze(object):
+    def __init__(self, fontPath):
+        self.font = ttLib.TTFont(fontPath)
+        self.kerningPairs = {}
+        self.singlePairs = {}
+        # self.firstGlyphsList = []
+        # self.firstGlyphsDict = {} # contains the first glyph of the pair, has two pairPosFormat keys, one for class kerning, the other for pair kerning
+        self.pairPosList = []
 
-def getClassKerning():
-    pass
+        if kGPOStableName not in self.font:
+            print "The font has no %s table" % kGPOStableName
+            self.goodbye()
 
 
-def run(fontPath):
-    font = ttLib.TTFont(fontPath)
-    
-    if kGPOStableName not in font:
-        print "The font has no %s table" % kGPOStableName
-        return
-    
-    gposTable = font[kGPOStableName].table
-    glyphPairsDict = {}
-    
-    # 'ScriptList:'
-    # scriptList = gposTable.ScriptList
+        self.analyzeFont()
+        self.findKerningLookups()
+        self.getPairPos()
+        self.getSinglePairs()
+        self.getClassPairs()
 
-    'FeatureList:'
-    featureList = gposTable.FeatureList
-    # featureCount = featureList.FeatureCount # integer
-    featureRecord = featureList.FeatureRecord
-    
-    uniqueKernLookupIndexList = collectUniqueKernLookupListIndexes(featureRecord)
-    
-    # Make sure a 'kern' feature was found:
-    if not len(uniqueKernLookupIndexList):
-        print "The font has no %s feature" % kKernFeatureTag
+
+    def goodbye(self):
+        print 'Aborted.'
         return
 
-    'LookupList:'
-    lookupList = gposTable.LookupList
-    for kernLookupIndex in sorted(uniqueKernLookupIndexList):
-        lookup = lookupList.Lookup[kernLookupIndex]
-        
-        '''
-        1   Single adjustment           Adjust position of a single glyph
-        2   Pair adjustment             Adjust position of a pair of glyphs
-        3   Cursive attachment          Attach cursive glyphs
-        4   MarkToBase attachment       Attach a combining mark to a base glyph
-        5   MarkToLigature attachment   Attach a combining mark to a ligature
-        6   MarkToMark attachment       Attach a combining mark to another mark
-        7   Context positioning         Position one or more glyphs in context
-        8   Chained Context positioning Position one or more glyphs in chained context
-        9   Extension positioning       Extension mechanism for other positionings
-        10+ Reserved                    For future use
-        '''
+
+    def analyzeFont(self):
+        self.gposTable = self.font[kGPOStableName].table
+
+        'ScriptList:'
+        self.scriptList = self.gposTable.ScriptList
+        'FeatureList:'
+        self.featureList = self.gposTable.FeatureList
+
+        self.featureCount = self.featureList.FeatureCount
+        self.featureRecord = self.featureList.FeatureRecord
+
+        self.uniqueKernLookupIndexList = collectUniqueKernLookupListIndexes(self.featureRecord)
 
 
-        # Confirm this is a GPOS LookupType 2; or using an extension table (GPOS LookupType 9):
-        if lookup.LookupType not in [2, 9]:
-            print "This is not a pair adjustment positioning lookup (GPOS LookupType 2); or using an extension table (GPOS LookupType 9)."
-            continue
-        
-        # Step through each subtable
-        for subtableItem in lookup.SubTable:
-            if subtableItem.LookupType == 2: # normal case, not using extension table
-                pairPos = subtableItem
-            elif subtableItem.LookupType == 9: # extension table
-                if subtableItem.ExtensionLookupType == 8: # contextual
-                    continue
-                elif subtableItem.ExtensionLookupType == 2:
-                    pairPos = subtableItem.ExtSubTable
-            
-            if pairPos.Coverage.Format not in [1, 2]:
-                print "WARNING: Coverage format %d is not yet supported" % pairPos.Coverage.Format
-            
-            if pairPos.ValueFormat1 not in [0, 4, 5]:
-                print "WARNING: ValueFormat1 format %d is not yet supported" % pairPos.ValueFormat1
-            
-            if pairPos.ValueFormat2 not in [0]:
-                print "WARNING: ValueFormat2 format %d is not yet supported" % pairPos.ValueFormat2
-            
-            # Each glyph in this list will have a corresponding PairSet which will
-            # contain all the second glyphs and the kerning value in the form of PairValueRecord(s)
-            firstGlyphsList = pairPos.Coverage.glyphs
-            print firstGlyphsList
+    def findKerningLookups(self):
+        if not len(self.uniqueKernLookupIndexList):
+            print "The font has no %s feature" % kKernFeatureTag
+            self.goodbye()
+
+        'LookupList:'
+        self.lookupList = self.gposTable.LookupList
+        self.lookups = []
+        for kernLookupIndex in sorted(self.uniqueKernLookupIndexList):
+            lookup = self.lookupList.Lookup[kernLookupIndex]
+
+            # Confirm this is a GPOS LookupType 2; or using an extension table (GPOS LookupType 9):
+
+            '''
+            Lookup types:
+            1   Single adjustment           Adjust position of a single glyph
+            2   Pair adjustment             Adjust position of a pair of glyphs
+            3   Cursive attachment          Attach cursive glyphs
+            4   MarkToBase attachment       Attach a combining mark to a base glyph
+            5   MarkToLigature attachment   Attach a combining mark to a ligature
+            6   MarkToMark attachment       Attach a combining mark to another mark
+            7   Context positioning         Position one or more glyphs in context
+            8   Chained Context positioning Position one or more glyphs in chained context
+            9   Extension positioning       Extension mechanism for other positionings
+            10+ Reserved                    For future use
+            '''
+
+            if lookup.LookupType not in [2, 9]:
+                print "This is not a pair adjustment positioning lookup (GPOS LookupType 2); or using an extension table (GPOS LookupType 9)."
+                continue
+            self.lookups.append(lookup)
+
+
+    def getPairPos(self):
+        for lookup in self.lookups:
+            for subtableItem in lookup.SubTable:
+
+                if subtableItem.LookupType == 2: # normal case, not using extension table
+                    pairPos = subtableItem
+
+                elif subtableItem.LookupType == 9: # extension table
+                    if subtableItem.ExtensionLookupType == 8: # contextual
+                        print 'Contextual Kerning not (yet?) supported.'
+                        continue
+                    elif subtableItem.ExtensionLookupType == 2:
+                        pairPos = subtableItem.ExtSubTable
+                
+                # print pairPos, pairPos.Format, pairPos.Coverage.Format 
+
+                # if pairPos.Coverage.Format not in [1, 2]:
+                if pairPos.Coverage.Format not in [1, 2]:
+                    print "WARNING: Coverage format %d is not yet supported" % pairPos.Coverage.Format
+                
+                if pairPos.ValueFormat1 not in [0, 4, 5]:
+                    print "WARNING: ValueFormat1 format %d is not yet supported" % pairPos.ValueFormat1
+                
+                if pairPos.ValueFormat2 not in [0]:
+                    print "WARNING: ValueFormat2 format %d is not yet supported" % pairPos.ValueFormat2
+
+
+                self.pairPosList.append(pairPos)
+                
+                # Each glyph in this list will have a corresponding PairSet which will
+                # contain all the second glyphs and the kerning value in the form of PairValueRecord(s)
+                # self.firstGlyphsDict[pairPos.Format] = pairPos.Coverage.glyphs
+
+                # self.firstGlyphsList.extend(pairPos.Coverage.glyphs)
+
+
+    def getSinglePairs(self):
+        for pairPos in self.pairPosList:
             if pairPos.Format == 1: 
-            
+                # single pair adjustment
+
+                firstGlyphsList = pairPos.Coverage.glyphs
+
                 # This iteration is done by index so that we have a way to reference the firstGlyphsList list
-                for pairSetIndex in range(len(pairPos.PairSet)):
+                for pairSetIndex, pairSetInstance in enumerate(pairPos.PairSet):
                     for pairValueRecordItem in pairPos.PairSet[pairSetIndex].PairValueRecord:
                         secondGlyph = pairValueRecordItem.SecondGlyph
                         valueFormat = pairPos.ValueFormat1
@@ -139,38 +176,47 @@ def run(fontPath):
                             print "\tValueFormat1 = %d" % valueFormat
                             continue # skip the rest
                         
-                        glyphPairsDict[(firstGlyphsList[pairSetIndex], secondGlyph)] = kernValue
+                        self.kerningPairs[(firstGlyphsList[pairSetIndex], secondGlyph)] = kernValue
+                        self.singlePairs[(firstGlyphsList[pairSetIndex], secondGlyph)] = kernValue
+
+                        # self.kerningPairs[(self.firstGlyphsDict[pairPos.Format][pairSetIndex], secondGlyph)] = kernValue
+                        # self.singlePairs[(self.firstGlyphsDict[pairPos.Format][pairSetIndex], secondGlyph)] = kernValue
 
 
-            ############################################
-            ############################################
-            ############################################
+    def getClassPairs(self):
+        for pairPos in self.pairPosList:
+            if pairPos.Format == 2: 
+                # class pair adjustment
 
+                firstGlyphsList = pairPos.Coverage.glyphs
 
-            elif pairPos.Format == 2: 
-            # class pair adjustment
                 firstGlyphs = {}
                 secondGlyphs = {}
 
                 leftClasses = {}
                 rightClasses = {}
                 
+
                 # Find left class with the Class1Record index="0".
-                # This class is weirdly mixed into the "Coverage" (e.g. all left glyphs) 
+                # This first class is mixed into the "Coverage" table (e.g. all left glyphs)
                 # and has no class="X" property, that is why we have to find them that way. 
                 
-                ## I THINK THE BUG IS IN HERE!
-                ###### XXXXXX
-
                 lg0 = myLeftClass()
-                for leftGlyph in firstGlyphsList:
-                    if not leftGlyph in pairPos.ClassDef1.classDefs:
-                        lg0.glyphs.append(leftGlyph)
+                # list of all glyphs kerned to the left of a pair, including all glyphs contained within kerning classes:
+                allLeftGlyphs = firstGlyphsList
+                # list of all glyphs contained within left-sided kerning classes:
+                allLeftClassGlyphs = pairPos.ClassDef1.classDefs.keys()
 
-                leftClasses[lg0.class1Record] = lg0        
-                
+                lg0.glyphs = list(set(allLeftGlyphs) - set(allLeftClassGlyphs))
+                lg0.glyphs.sort()
+
+                leftClasses[lg0.class1Record] = lg0
+
+
+                                
+
+
                 # Find all the remaining left classes:
-                
                 for leftGlyph in pairPos.ClassDef1.classDefs:
                     class1Record = pairPos.ClassDef1.classDefs[leftGlyph]
                     if class1Record in leftClasses:
@@ -194,10 +240,6 @@ def run(fontPath):
                         rightClasses[class2Record].glyphs.append(rightGlyph)
                     
 
-                
-                # for i in rightClasses:
-                #     print rightClasses[i].class2Record, sorted(rightClasses[i].glyphs)
-
                 for record_l in leftClasses:
                     for record_r in rightClasses:
                         if pairPos.Class1Record[record_l].Class2Record[record_r]:
@@ -214,58 +256,50 @@ def run(fontPath):
                             if kernValue != 0:
                                 for l in leftClasses[record_l].glyphs:
                                     for r in rightClasses[record_r].glyphs:
-                                        if (l, r) in glyphPairsDict:
+                                        if (l, r) in self.kerningPairs:
                                             # if the kerning pair has already been assigned in pair-to-pair kerning
                                             continue
                                         else:
                                             if valueFormat == 5: # RTL kerning
                                                 kernValue = "<%d 0 %d 0>" % (pairPos.Class1Record[record_l].Class2Record[record_r].Value1.XPlacement, pairPos.Class1Record[record_l].Class2Record[record_r].Value1.XAdvance)
                                             
-                                            glyphPairsDict[(l, r)] = kernValue
+
+                                            self.kerningPairs[(l, r)] = kernValue
                             
                         else:
                             print 'ERROR'
 
-    # print '-' * 80
-    # for i in sorted(glyphPairsDict.keys()):
-    #     print i[0], i[1], glyphPairsDict[i]
-    # print
-    # print len(glyphPairsDict)
-    
-    for pair, value in glyphPairsDict.items():
-         finalList.append('/%s /%s %s' % ( pair[0], pair[1], value ))
-        # AFMlist.append('KPX %s %s %s' % ( pair[0], pair[1], value ))
-        
-    finalList.sort()
-    # AFMlist.sort()
-    output = '\n'.join(finalList)
-    # print output
-    print len(output)
-
-    # AFMoutput = '\n'.join(AFMlist)
-    # scrap = os.popen('pbcopy', 'w')
-    # scrap.write(AFMoutput)
-    # scrap.close()
-    # print 'Done. AFM copied to clipboard.'
-
-#     scrap = os.popen('pbcopy', 'w')
-#     scrap.write(output)
-#     scrap.close()
-#    print 'done'
 
 
-#     membersList = inspect.getmembers(pairPos.ClassDef1.classDefs)
-#     for x in membersList:
-#         print x
-#     print
-#     print
-    
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         if os.path.exists(sys.argv[1]):
             fontPath = sys.argv[1]
-            run(fontPath)
+            f = Analyze(fontPath)
+
+            # for c in f.leftClasses:
+            #     print 'leftClass:', c, f.leftClasses[c].glyphs
+
+            # for c in f.rightClasses:
+            #     print 'rightClass:', c, f.rightClasses[c].glyphs
+
+
+            # for x in f.lookups:
+            #     print x
+
+            finalList = []
+            for pair, value in f.kerningPairs.items():
+                finalList.append('/%s /%s %s' % ( pair[0], pair[1], value ))
+
+            finalList.sort()
+
+            output = '\n'.join(finalList)
+            print output
+
+            print len(f.kerningPairs)
+            # print len(f.singlePairs)
+
     else:
         print "No valid font provided."
