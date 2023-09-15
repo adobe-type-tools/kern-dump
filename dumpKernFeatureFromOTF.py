9330b90076bd0233ac97f9e24837b84c6c5cd8ca
Frank Grießhammer <fgriessh@adobe.com>
2023-09-15 17:10:35 +0200

    modernization, testability

--- dumpKernFeatureFromOTF.py
+++ dumpKernFeatureFromOTF.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
+import argparse
 import os
-import sys
 import string
 from importlib import reload
+from pathlib import Path
 
 import getKerningPairsFromOTF
 reload(getKerningPairsFromOTF)
 
-__doc__ = '''\
+__doc__ = '''
 
     This script extracts a viable kern feature file from a compiled OTF.
     It requires the script 'getKerningPairsFromOTF.py'; which is distributed
@@ -18,7 +19,6 @@ __doc__ = '''\
 
     '''
 
-kKernFeatureTag = 'kern'
 compressSinglePairs = True
 # Switch to control if single pairs shall be written plainly, or in a more
 # space-saving notation (using enum pos).
@@ -57,7 +57,7 @@ def nameClass(glyphlist, flag):
 
     flag = flag
 
-    return '@%s%s%s' % (name, flag, case)
+    return f'@{name}{flag}{case}'
 
 
 def buildOutputList(sourceList, outputList, headlineString):
@@ -74,41 +74,41 @@ def buildOutputList(sourceList, outputList, headlineString):
 
 
 def makeKernFeature(fontPath):
-    f = getKerningPairsFromOTF.OTFKernReader(fontPath)
+    okr = getKerningPairsFromOTF.OTFKernReader(fontPath)
     allClasses = {}
     classList = []
-    output = []
+    fea_output = []
 
-    for kerningClass in f.allLeftClasses:
-        glyphs = sortGlyphs(f.allLeftClasses[kerningClass])
+    for kerningClass in okr.allLeftClasses:
+        glyphs = sortGlyphs(okr.allLeftClasses[kerningClass])
         className = nameClass(glyphs, '_LEFT')
         allClasses.setdefault(className, glyphs)
 
-    for kerningClass in f.allRightClasses:
-        glyphs = sortGlyphs(f.allRightClasses[kerningClass])
+    for kerningClass in okr.allRightClasses:
+        glyphs = sortGlyphs(okr.allRightClasses[kerningClass])
         className = nameClass(glyphs, '_RIGHT')
         allClasses.setdefault(className, glyphs)
 
-    singlePairsList = sorted(f.singlePairs.items())
+    singlePairsList = sorted(okr.singlePairs.items())
 
     classPairsList = []
-    for (leftClass, rightClass), value in sorted(f.classPairs.items()):
-        leftGlyphs = sortGlyphs(f.allLeftClasses[leftClass])
+    for (leftClass, rightClass), value in sorted(okr.classPairs.items()):
+        leftGlyphs = sortGlyphs(okr.allLeftClasses[leftClass])
         leftClassName = nameClass(leftGlyphs, '_LEFT')
 
-        rightGlyphs = sortGlyphs(f.allRightClasses[rightClass])
+        rightGlyphs = sortGlyphs(okr.allRightClasses[rightClass])
         rightClassName = nameClass(rightGlyphs, '_RIGHT')
 
         classPairsList.append(((leftClassName, rightClassName), value))
 
     for className in sorted(allClasses):
         glyphs = allClasses[className]
-        classList.append('%s = [ %s ];' % (className, ' '.join(glyphs)))
+        classList.append(f'{className} = [ {" ".join(glyphs)} ];')
 
     buildOutputList(
-        [], output, 'kern feature dumped from %s' % os.path.basename(fontPath))
+        [], fea_output, f'kern feature dumped from {os.path.basename(fontPath)}')
     buildOutputList(
-        classList, output, 'kerning classes')
+        classList, fea_output, 'kerning classes')
 
     if compressSinglePairs:
 
@@ -159,63 +159,73 @@ def makeKernFeature(fontPath):
         # lists; organized by type:
 
         for left, right, value in compressedBoth:
+            left_items = ' '.join(left)
+            right_items = ' '.join(right)
             if len(left) != 1 and len(right) != 1:
                 exploding_class_class.append(
-                    'enum pos [ %s ] [ %s ] %s;' % (' '.join(left), ' '.join(right), value))
+                    f'enum pos [ {left_items} ] [ {right_items} ] ;')
             elif len(left) != 1 and len(right) == 1:
                 class_glyph.append(
-                    'enum pos [ %s ] %s %s;' % (' '.join(left), ' '.join(right), value))
+                    f'enum pos [ {left_items} ] {right_items} {value};')
             elif len(left) == 1 and len(right) != 1:
                 glyph_class.append(
-                    'enum pos %s [ %s ] %s;' % (' '.join(left), ' '.join(right), value))
+                    f'enum pos {left_items} [ {right_items} ] {value};')
             elif len(left) == 1 and len(right) == 1:
                 glyph_glyph.append(
-                    'pos %s %s %s;' % (' '.join(left), ' '.join(right), value))
+                    f'pos {left_items} {right_items} {value};')
             else:
-                print('ERROR with (%s)' % (' '.join(left, right, value)))
+                print(f'ERROR with ({" ".join(left, right, value)})')
 
         # Making sure all the pairs made it through the process:
-        if len(compressedBoth) != len(class_glyph) + len(glyph_class) + len(glyph_glyph) + len(exploding_class_class):
+        if len(compressedBoth) != (
+            len(class_glyph) + len(glyph_class) +
+            len(glyph_glyph) + len(exploding_class_class)
+        ):
             print('ERROR - we lost some kerning pairs.')
 
-        buildOutputList(glyph_glyph, output, 'glyph to glyph')
-        buildOutputList(glyph_class, output, 'glyph to class')
-        buildOutputList(class_glyph, output, 'class to glyph')
-        buildOutputList(exploding_class_class, output, 'exploding class to exploding class')
+        buildOutputList(glyph_glyph, fea_output, 'glyph to glyph')
+        buildOutputList(glyph_class, fea_output, 'glyph to class')
+        buildOutputList(class_glyph, fea_output, 'class to glyph')
+        buildOutputList(
+            exploding_class_class, fea_output,
+            'exploding class to exploding class')
 
     else:
         # Plain list of single pairs
         glyph_glyph = []
         for (left, right), value in singlePairsList:
-            glyph_glyph.append('pos %s %s %s;' % (left, right, value))
+            glyph_glyph.append(f'pos {left} {right} {value};')
 
-        buildOutputList(glyph_glyph, output, 'glyph to glyph')
+        buildOutputList(glyph_glyph, fea_output, 'glyph to glyph')
 
     # List of class-to-class pairs
     class_class = []
     for (left, right), value in classPairsList:
-        class_class.append('pos %s %s %s;' % (left, right, value))
+        class_class.append(f'pos {left} {right} {value};')
 
-    buildOutputList(class_class, output, 'class to class')
-    print('\n'.join(output))
+    buildOutputList(class_class, fea_output, 'class to class')
+    return fea_output
+
+
+def get_args(args=None):
+
+    parser = argparse.ArgumentParser(
+        description=__doc__
+    )
+    parser.add_argument(
+        'font_file',
+        metavar='FONT',
+        help='font file',
+    )
+    return parser.parse_args(args)
 
 
 def main():
-
-    if len(sys.argv) == 2:
-        assumedFontPath = sys.argv[1]
-
-        if (
-            os.path.exists(assumedFontPath) and
-            os.path.splitext(assumedFontPath)[1].lower() in ['.otf', '.ttf']
-        ):
-            fontPath = sys.argv[1]
-            makeKernFeature(fontPath)
-        else:
-            print("No valid font provided.")
-
-    else:
-        print("No valid font provided.")
+    args = get_args()
+    font_path = Path(args.font_file)
+    if font_path.exists() and font_path.suffix in ['.otf', '.ttf']:
+        fea = makeKernFeature(font_path)
+        print('\n'.join(fea))
 
 
 if __name__ == "__main__":
